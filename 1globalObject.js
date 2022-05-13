7433a147c08f8ace4d892b8560dd40344003f50a
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-05-13 22:47:13 +0530

    Update 1globalObject.js

--- 1globalObject.js
+++ 1globalObject.js
@@ -1,4 +1,7 @@
 var x=3;
 console.log(x);
 //We have 3 types of global in Node.js
+//process
+//global
+//buffer
 // console.log is a global Object also setTimeout, clearTimeout, clearInterval and setInterval
