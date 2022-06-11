6b16f10ead2554494c0d42a6d421aeaeab99cf77
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-06-11 23:30:58 +0530

    Update 1globalObject.js

--- 1globalObject.js
+++ 1globalObject.js
@@ -2,6 +2,7 @@ var x=3;
 console.log(x);
 //We have 3 types of global in Node.js
 //process----> we can access it from anywhere
-//global---> 
+//global---> Golobal is the global object in Node.JS
 //buffer----->
 // console.log is a global Object also setTimeout, clearTimeout, clearInterval and setInterval
+//
