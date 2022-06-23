1b6f21dc9b732f97b6291419db4b7916a82937da
Mohd Aliyan <71941136+MohdAliyan27@users.noreply.github.com>
2022-06-23 23:12:00 +0530

    Update 6_mail.js

--- 6_mail.js
+++ 6_mail.js
@@ -1,29 +1,28 @@
 
 var nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-service: ‘gmail’,
-host: ‘smtp.gmail.com’,
-secure: ‘true’,
-port: ‘465’,
-auth: {
-user: 'youremail@gmail.com',
-pass: 'yourpassword'
+let transporter = nodemailer.createTransport(
+{
+     service: ‘gmail’,
+     host: ‘smtp.gmail.com’,
+     secure: ‘true’,
+     port: ‘465’,
+     auth: {
+          user: 'test2703@gmail.com',
+          pass: 'yourpasswordTest123@'
      }
 });
 
 let mailOptions = {
-
             from: '"Your Name" <youremail@example.com>', // sender address
             to: 'myfriend@gmail.com, // list of receivers
             subject: 'Sending Email using Node.js', // Subject line
             text: 'Hello world', // plain text body
-          
-        };
+};
 
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
     console.log(error);
   } else {
     console.log('Email sent: ' + info.response);
-  }
+ }
